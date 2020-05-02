34b4b46b8f29922b855607ecdb190dbf713656a0
François-Xavier Lyonnet du Moutier <fx.du.moutier@gmail.com>
2020-05-02 04:06:17 +0200

    standard euro format

--- deck.rb
+++ deck.rb
@@ -3,17 +3,18 @@ require 'game_icons'
 
 card_type = []
 
+deck = Squib.xlsx file: 'mecaniscout.xlsx'
 
-Squib::Deck.new(cards: 78,layout: 'layout.yml') do
+Squib::Deck.new(cards: deck['Titre'].size, width: '59mm', height: '92mm', layout: 'layout.yml') do
   #background color: '#230602'
-  deck = xlsx file: 'mecaniscout.xlsx'
   icones = deck['Icone'].map { |x|  GameIcons.get(x).recolor(bg_opacity: 0, fg: 'FFFFFF' ).string} 
   frames = deck['Type'].map {|x| "Frame#{x}"}
   types = deck['Type'].map {|x| "Type#{x}"}
 
   cut_zone radius: 25,  stroke_color: :white
-  safe_zone radius: 0, stroke_color: :red
+  rect layout: :outerframe
   rect layout: frames
+  #safe_zone radius: 0, stroke_color: :red
   rect layout: :DescRectShadow
   rect layout: :DescRect
 
@@ -29,6 +30,6 @@ Squib::Deck.new(cards: 78,layout: 'layout.yml') do
 
 
   svg data: icones, layout: :Icone
-
-  save format: :pdf
+  
+  save_pdf sprue: 'a4_euro_card.yml'
 end
