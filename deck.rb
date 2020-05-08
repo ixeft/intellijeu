842887af975aa074aff5665715c1473aca534354
François-Xavier Lyonnet du Moutier <fx.du.moutier@gmail.com>
2020-05-08 17:58:58 +0200

    remove level for beta version

--- deck.rb
+++ deck.rb
@@ -19,14 +19,14 @@ Squib::Deck.new(cards: deck['Titre'].size, width: '59mm', height: '92mm', layout
   rect layout: :DescRect
 
   %w(Titre Description Exemple).each do |key|
-    rect layout: key
+    #rect layout: key
     text str: deck[key], layout: key
   end
 
   svg layout: types 
 
-  niveau = deck['Niveau'].map {|x| "Niveau#{x}"}
-  svg layout: niveau
+  #niveau = deck['Niveau'].map {|x| "Niveau#{x}"}
+  #svg layout: niveau
 
 
   svg data: icones, layout: :Icone
